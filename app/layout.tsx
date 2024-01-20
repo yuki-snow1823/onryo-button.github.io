6e8accbfb5ac2fdb67eac7f36f705cdefadef90b
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-01-20 14:26:39 +0900

    一旦戻した

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "怨霊ボタン",
   description: "そのボタンは押してはならない...",
+  metadataBase: new URL('https://yuki-snow1823.github.io/onryo-button.github.io/'),
 };
 
 export default function RootLayout({
@@ -19,10 +20,6 @@ export default function RootLayout({
     <html lang="ja">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta property="og:url" content="https://yuki-snow1823.github.io/onryo-button.github.io/" />
-        <meta property="og:image" content="https://github.com/yuki-snow1823/onryo-button.github.io/assets/59280290/effbb6f7-f867-49fc-8316-a8721780deef" />
-        <meta name="twitter:img:src" content="https://github.com/yuki-snow1823/onryo-button.github.io/assets/59280290/effbb6f7-f867-49fc-8316-a8721780deef" />
-        <meta name="x:img:src" content="https://github.com/yuki-snow1823/onryo-button.github.io/assets/59280290/effbb6f7-f867-49fc-8316-a8721780deef" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
