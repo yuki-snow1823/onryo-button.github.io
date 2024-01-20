f351b921c56638d4d5ed1231ebb9ad64e79d7679
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-01-20 14:15:41 +0900

    URLが必要？

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
     <html lang="ja">
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="og:url" content="https://yuki-snow1823.github.io/onryo-button.github.io/" />
         <meta property="og:image" content="https://github.com/yuki-snow1823/onryo-button.github.io/assets/59280290/effbb6f7-f867-49fc-8316-a8721780deef" />
       </Head>
       <body className={inter.className}>{children}</body>
