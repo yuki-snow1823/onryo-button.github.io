45974e2c41411fd38cd7c59f3e9cfb794aae8c2d
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-01-20 14:02:01 +0900

    URL設定書く場所間違えてた

--- next.config.js
+++ next.config.js
@@ -4,9 +4,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  generateMetadata: {
-    metadataBase: 'https://yuki-snow1823.github.io/onryo-button.github.io/',
-  },
 }
 
 module.exports = nextConfig
