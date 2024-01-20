9efea250a2ac27a536fe9c85685731778a4e763e
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2024-01-20 14:22:33 +0900

    こう？

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,8 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="og:url" content="https://yuki-snow1823.github.io/onryo-button.github.io/" />
         <meta property="og:image" content="https://github.com/yuki-snow1823/onryo-button.github.io/assets/59280290/effbb6f7-f867-49fc-8316-a8721780deef" />
+        <meta name="twitter:img:src" content="https://github.com/yuki-snow1823/onryo-button.github.io/assets/59280290/effbb6f7-f867-49fc-8316-a8721780deef" />
+        <meta name="x:img:src" content="https://github.com/yuki-snow1823/onryo-button.github.io/assets/59280290/effbb6f7-f867-49fc-8316-a8721780deef" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
